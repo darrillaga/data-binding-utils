6ec080f9832261df56ae9d051cd1d95afb28ae4d
Damián Arrillaga <damian.arrillaga@gmail.com>
2016-07-17 17:55:40 -0300

    Add synchronization to ObservableDispatcherList

--- databindingutils/src/main/java/me/darrillaga/databinding/utils/ObservableDispatcherList.java
+++ databindingutils/src/main/java/me/darrillaga/databinding/utils/ObservableDispatcherList.java
@@ -6,6 +6,7 @@ import android.os.Looper;
 import android.support.annotation.NonNull;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
@@ -17,10 +18,12 @@ public class ObservableDispatcherList<T> implements ObservableList<T> {
     }
 
     private ObservableList<T> mList;
+    private List<T> mSynchronizedList;
     private DispatcherWrapper mDispatcherWrapper;
 
     public ObservableDispatcherList(ObservableList<T> list, Dispatcher dispatcher) {
         mList = list;
+        mSynchronizedList = Collections.synchronizedList(mList);
         mDispatcherWrapper = new DispatcherWrapper(dispatcher);
     }
 
@@ -36,131 +39,131 @@ public class ObservableDispatcherList<T> implements ObservableList<T> {
 
     @Override
     public void add(int location, T object) {
-        mDispatcherWrapper.execute(() -> mList.add(location, object));
+        mDispatcherWrapper.execute(() -> mSynchronizedList.add(location, object));
     }
 
     @Override
     public boolean add(T object) {
-        mDispatcherWrapper.execute(() -> mList.add(object));
+        mDispatcherWrapper.execute(() -> mSynchronizedList.add(object));
         return true;
     }
 
     @Override
     public boolean addAll(int location, Collection<? extends T> collection) {
-        mDispatcherWrapper.execute(() -> mList.addAll(location, collection));
+        mDispatcherWrapper.execute(() -> mSynchronizedList.addAll(location, collection));
         return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends T> collection) {
-        mDispatcherWrapper.execute(() -> mList.addAll(collection));
+        mDispatcherWrapper.execute(() -> mSynchronizedList.addAll(collection));
         return true;
     }
 
     @Override
     public void clear() {
-        mDispatcherWrapper.execute(() -> mList.clear());
+        mDispatcherWrapper.execute(() -> mSynchronizedList.clear());
     }
 
     @Override
     public boolean contains(Object object) {
-        return mList.contains(object);
+        return mSynchronizedList.contains(object);
     }
 
     @Override
     public boolean containsAll(Collection<?> collection) {
-        return mList.containsAll(collection);
+        return mSynchronizedList.containsAll(collection);
     }
 
     @Override
     public T get(int location) {
-        return mList.get(location);
+        return mSynchronizedList.get(location);
     }
 
     @Override
     public int indexOf(Object object) {
-        return mList.indexOf(object);
+        return mSynchronizedList.indexOf(object);
     }
 
     @Override
     public boolean isEmpty() {
-        return mList.isEmpty();
+        return mSynchronizedList.isEmpty();
     }
 
     @NonNull
     @Override
     public Iterator<T> iterator() {
-        return mList.iterator();
+        return mSynchronizedList.iterator();
     }
 
     @Override
     public int lastIndexOf(Object object) {
-        return mList.lastIndexOf(object);
+        return mSynchronizedList.lastIndexOf(object);
     }
 
     @Override
     public ListIterator<T> listIterator() {
-        return mList.listIterator();
+        return mSynchronizedList.listIterator();
     }
 
     @NonNull
     @Override
     public ListIterator<T> listIterator(int location) {
-        return mList.listIterator(location);
+        return mSynchronizedList.listIterator(location);
     }
 
     @Override
     public T remove(int location) {
         T removed = get(location);
-        mDispatcherWrapper.execute(() -> mList.remove(location));
+        mDispatcherWrapper.execute(() -> mSynchronizedList.remove(location));
         return removed;
     }
 
     @Override
     public boolean remove(Object object) {
         boolean exists = contains(object);
-        mDispatcherWrapper.execute(() -> mList.remove(object));
+        mDispatcherWrapper.execute(() -> mSynchronizedList.remove(object));
         return exists;
     }
 
     @Override
     public boolean removeAll(Collection<?> collection) {
-        mDispatcherWrapper.execute(() -> mList.removeAll(collection));
+        mDispatcherWrapper.execute(() -> mSynchronizedList.removeAll(collection));
         return true;
     }
 
     @Override
     public boolean retainAll(Collection<?> collection) {
-        mDispatcherWrapper.execute(() -> mList.retainAll(collection));
+        mDispatcherWrapper.execute(() -> mSynchronizedList.retainAll(collection));
         return true;
     }
 
     @Override
     public T set(int location, T object) {
-        mDispatcherWrapper.execute(() -> mList.set(location, object));
+        mDispatcherWrapper.execute(() -> mSynchronizedList.set(location, object));
         return null;
     }
 
     @Override
     public int size() {
-        return mList.size();
+        return mSynchronizedList.size();
     }
 
     @NonNull
     @Override
     public List<T> subList(int start, int end) {
-        return mList.subList(start, end);
+        return mSynchronizedList.subList(start, end);
     }
 
     @NonNull
     @Override
     public Object[] toArray() {
-        return mList.toArray();
+        return mSynchronizedList.toArray();
     }
 
     @NonNull
     @Override
     public <T1> T1[] toArray(T1[] array) {
-        return mList.toArray(array);
+        return mSynchronizedList.toArray(array);
     }
 }
